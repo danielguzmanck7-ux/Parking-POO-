a2e407d8d5b2e39517ec9cc57da18245c939b596
Daniel Guzman <danielguzmanck7@gmail.com>
2026-02-10 19:33:11 +0100

    inicio

---
+++ Ejercicios/Parking.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import java.util.*;
+
+public class Parking {
+
+      public parking (String nombre, int matriculas){
+
+      ArrayList<Integer> matriculas = new ArrayList<>();
+}
+}
---
+++ Ejercicios/ParkingException.java
@@ -0,0 +1,13 @@
+public class ParkingException extends Exception {
+
+      private final String mensaje;
+      private final String matricula;
+
+      public ParkingException (String mensaje, String matricula){
+            this.mensaje = mensaje;
+            this.matricula = matricula;
+      }
+
+      
+
+}
