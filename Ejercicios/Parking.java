1c42d320e90f2172bcab0a0be6222cee185530c5
Daniel Guzman <danielguzmanck7@gmail.com>
2026-02-23 20:21:03 +0100

    1er dia.1

--- Ejercicios/Parking.java
+++ Ejercicios/Parking.java
@@ -5,49 +5,101 @@ import java.util.*;
 public class Parking {
 
       private ArrayList<String> matriculas;
-      private String nombre; 
+      private String nombre;
 
-      public Parking (String nombre, int nPlazas){
+      public Parking(String nombre, int nPlazas) {
 
-      this.nombre = nombre;
-      matriculas = new ArrayList<String>();
+            this.nombre = nombre;
+            matriculas = new ArrayList<String>();
 
-      for(int i = 0 ; i < nPlazas; i++){
-            matriculas.add(null);
+            for (int i = 0; i < nPlazas; i++) {
+                  matriculas.add(null);
+            }
       }
-}
 
-      public String getNombre(){
+      public String getNombre() {
             return nombre;
       }
 
-      public void entrada (String matricula, int plaza ) throws ParkingException{
+      public void entrada(String matricula, int plaza) throws ParkingException {
 
-            if (matricula == null || matricula.length() < 4){
+            if (matricula == null || matricula.length() < 4) {
                   throw new ParkingException(" matricula incorrecta ", matricula);
             }
-            if (plaza < 0  || plaza >= matriculas.size()){
+            if (plaza < 0 || plaza >= matriculas.size()) {
                   throw new ParkingException(" plaza inexistente ", matricula);
             }
-            if (matriculas.get(plaza) != null){
+            if (matriculas.get(plaza) != null) {
                   throw new ParkingException(" plaza ocupada ", matricula);
             }
-            if (matriculas.contains(matricula)){
+            if (matriculas.contains(matricula)) {
                   throw new ParkingException(" matricula repetida ", matricula);
             }
             matriculas.set(plaza, matricula);
       }
 
-      public int salida(String matricula) throws ParkingException{
+      public int salida(String matricula) throws ParkingException {
 
             int plazaLiberada = -1;
 
-            if (!matriculas.contains(matricula)){
+            if (!matriculas.contains(matricula)) {
                   throw new ParkingException(" matricula no existente ", matricula);
             }
-            // Este de abajo es una forma mas simple del FOR, utiliza indexOf que es un metodo encargado de devolver el lugar donde se encuentra esa matricula especifica. Despues de eso simplemente coges ese sitio, lo pones a nulo y despues lo devuelves  
-            /* int plaza = matriculas.indexOf(matricula);
-            matriculas.set(plaza,null);
-            return plaza;*/
+            // Este de abajo es una forma mas simple del FOR, utiliza indexOf que es un
+            // metodo encargado de devolver el lugar donde se encuentra esa matricula
+            // especifica. Despues de eso simplemente coges ese sitio, lo pones a nulo y
+            // despues lo devuelves
+            /*
+             * int plaza = matriculas.indexOf(matricula);
+             * matriculas.set(plaza,null);
+             * return plaza;
+             */
+
+            // recorremos todo el array list y despes miramos cada una de las matriculas que
+            // tiene adentro
+            for (int i = 0; i < matriculas.size(); i++) {
+                  if (matricula.equals(matriculas.get(i))) {
+                        matriculas.set(i, null);
+                        plazaLiberada = i;
+                  }
+            }
+            return plazaLiberada;
+      }
+
+      public int getPlazasTotales() {
+            return matriculas.size();
+      }
+
+      // lo recorremos y vemosque en el espacio no haya null, si hay null pues
+      // seguimos, si no hay pues agregamos 1
+      public int getPlazasOcupadas() {
+            int ocupadas = 0;
+
+            for (String m : matriculas) {
+                  if (m != null)
+                        ;
+                  ocupadas++;
+            }
+            return ocupadas;
+      }
+
+      public int getPlazasPlazasLibres(){
+            return getPlazasTotales() - getPlazasOcupadas();
+      }
+
+      @Override
+      public String toString(){
+
+            String resultado = nombre + "\n";
+            resultado += "---------------\n";
+
+            for (int i = 0 ; i < matriculas.size() ; i++){
+                  if(matriculas.get(i) == null){
+                        resultado += "Plaza" + i + ": (Vacia)\n";
+                  }else{
+                        resultado += "Plaza" + i + matriculas.get(i) + "\n";
+                  }
+            }
+            return resultado;
       }
 }
---
+++ Ejercicios/Principal.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Principal {
+    
+public static void main(String[] args) {
+      
+      Scanner sc = new Scanner(System.in);
+      Parking parking = new Parking("Parking Centro", 10);
+
+      int opcion = 0;
+
+      do{
+            System.out.println("\n--- Menu ---");
+            System.out.println("1. Entrar ");
+            System.out.println("2. Salida Coche ");
+            System.out.println("3. Mostrar");
+            System.out.println("4. Salir ");
+            System.out.println("Seleccione una opcion");
+
+            switch(opcion){
+                  case 1:
+                        try{
+
+                        }catch{
+
+                        }
+                        break;
+
+                  case 2:
+                        try{
+
+                        }catch{
+
+                        }
+                        break;
+                  case 3:
+                        try{
+
+                        }catch{
+
+                        }
+                        break;
+                  case 4:
+                        try{
+
+                        }catch{
+
+                        }
+                        break;
+
+                  default:
+
+        }
+
+      }while (opcion != 4);
+      
+      sc.close();
+  }
+}
