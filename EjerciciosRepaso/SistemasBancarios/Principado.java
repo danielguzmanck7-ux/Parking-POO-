4812926f34ba7e0bb0a7eb7568a3a348b40b66a4
Daniel Guzman <danielguzmanck7@gmail.com>
2026-03-02 16:42:21 +0100

    sa

--- EjerciciosRepaso/SistemasBancarios/Principado.java
+++ EjerciciosRepaso/SistemasBancarios/Principado.java
@@ -9,7 +9,7 @@ public class Principado {
                   System.out.println(" -- Saldo Inicial -- " + cuenta.getSaldo());
 
                   cuenta.retirar(100.6);
-                  cuenta.ingresar(25.4);a
+                  cuenta.ingresar(25.4);
 
                   System.out.println(" -- Saldo Final -- " + cuenta.getSaldo());
 
